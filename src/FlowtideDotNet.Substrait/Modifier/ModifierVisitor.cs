e476814646135a241f353f02d5c504df0b354dfe
Ulimo <alexander.ostman@hotmail.com>
2024-02-18 23:29:23 +0100

    Remove code duplication

--- src/FlowtideDotNet.Substrait/Modifier/ModifierVisitor.cs
+++ src/FlowtideDotNet.Substrait/Modifier/ModifierVisitor.cs
@@ -14,7 +14,7 @@ using FlowtideDotNet.Substrait.Relations;
 
 namespace FlowtideDotNet.Substrait.Modifier
 {
-    internal class ModifierVisitor : RelationVisitor<Relation, object?>
+    internal class ModifierVisitor : BaseModifierVisitor
     {
         private readonly Dictionary<string, ReferenceInfo> subPlanNameToId;
 
@@ -34,126 +34,6 @@ namespace FlowtideDotNet.Substrait.Modifier
                 };
             }
             return readRelation;
-        }
-
-        public override Relation VisitFilterRelation(FilterRelation filterRelation, object? state)
-        {
-            filterRelation.Input = Visit(filterRelation.Input, state);
-            return filterRelation;
-        }
-
-        public override Relation VisitJoinRelation(JoinRelation joinRelation, object? state)
-        {
-            joinRelation.Left = Visit(joinRelation.Left, state);
-            joinRelation.Right = Visit(joinRelation.Right, state);
-            return joinRelation;
-        }
-
-        public override Relation VisitNormalizationRelation(NormalizationRelation normalizationRelation, object? state)
-        {
-            normalizationRelation.Input = Visit(normalizationRelation.Input, state);
-            return normalizationRelation;
-        }
-
-        public override Relation VisitProjectRelation(ProjectRelation projectRelation, object? state)
-        {
-            projectRelation.Input = Visit(projectRelation.Input, state);
-            return projectRelation;
-        }
-
-        public override Relation VisitWriteRelation(WriteRelation writeRelation, object? state)
-        {
-            writeRelation.Input = Visit(writeRelation.Input, state);
-            return writeRelation;
-        }
-
-        public override Relation VisitPlanRelation(PlanRelation planRelation, object? state)
-        {
-            return planRelation;
-        }
-
-        public override Relation VisitReferenceRelation(ReferenceRelation referenceRelation, object? state)
-        {
-            return referenceRelation;
-        }
-
-        public override Relation VisitRootRelation(RootRelation rootRelation, object? state)
-        {
-            rootRelation.Input = Visit(rootRelation.Input, state);
-            return rootRelation;
-        }
-
-        public override Relation VisitSetRelation(SetRelation setRelation, object? state)
-        {
-            for (int i = 0; i < setRelation.Inputs.Count; i++)
-            {
-                setRelation.Inputs[i] = Visit(setRelation.Inputs[i], state);
-            }
-            return setRelation;
-        }
-
-        public override Relation VisitMergeJoinRelation(MergeJoinRelation mergeJoinRelation, object? state)
-        {
-            mergeJoinRelation.Left = Visit(mergeJoinRelation.Left, state);
-            mergeJoinRelation.Right = Visit(mergeJoinRelation.Right, state);
-            return mergeJoinRelation;
-        }
-
-        public override Relation VisitAggregateRelation(AggregateRelation aggregateRelation, object? state)
-        {
-            aggregateRelation.Input = Visit(aggregateRelation.Input, state);
-            return aggregateRelation;
-        }
-
-        public override Relation VisitIterationRelation(IterationRelation iterationRelation, object? state)
-        {
-            if (iterationRelation.Input != null)
-            {
-                iterationRelation.Input = Visit(iterationRelation.Input, state);
-            }
-            iterationRelation.LoopPlan = Visit(iterationRelation.LoopPlan, state);
-            
-            return iterationRelation;
-        }
-
-        public override Relation VisitIterationReferenceReadRelation(IterationReferenceReadRelation iterationReferenceReadRelation, object? state)
-        {
-            return iterationReferenceReadRelation;
-        }
-
-        public override Relation VisitUnwrapRelation(UnwrapRelation unwrapRelation, object? state)
-        {
-            unwrapRelation.Input = Visit(unwrapRelation.Input, state);
-            return unwrapRelation;
-        }
-
-        public override Relation VisitVirtualTableReadRelation(VirtualTableReadRelation virtualTableReadRelation, object? state)
-        {
-            return virtualTableReadRelation;
-        }
-
-        public override Relation VisitBufferRelation(BufferRelation bufferRelation, object? state)
-        {
-            bufferRelation.Input = Visit(bufferRelation.Input, state);
-            return bufferRelation;
-        }
-
-        public override Relation VisitFetchRelation(FetchRelation fetchRelation, object? state)
-        {
-            fetchRelation.Input = Visit(fetchRelation.Input, state);
-            return fetchRelation;
-        }
-
-        public override Relation VisitSortRelation(SortRelation sortRelation, object? state)
-        {
-            sortRelation.Input = Visit(sortRelation.Input, state);
-            return sortRelation;
-        }
-
-        public override Relation VisitTopNRelation(TopNRelation topNRelation, object? state)
-        {
-            topNRelation.Input = Visit(topNRelation.Input, state);
-            return topNRelation;
         }
     }
 }
