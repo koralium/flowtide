66db0293d222ac94a3798ad5aad7812f16f45c94
Alexander Östman <alexander.ostman@hotmail.com>
2024-05-03 10:40:39 +0200

    Fix table provider

--- src/FlowtideDotNet.Connector.Sharepoint/Internal/SharepointTableProvider.cs
+++ src/FlowtideDotNet.Connector.Sharepoint/Internal/SharepointTableProvider.cs
@@ -26,11 +26,14 @@ namespace FlowtideDotNet.Connector.Sharepoint.Internal
     internal class SharepointTableProvider : ITableProvider
     {
         private readonly SharepointSourceOptions _sharepointSourceOptions;
+        private readonly Dictionary<string, TableMetadata> _tables = new Dictionary<string, TableMetadata>();
+        private readonly GraphServiceClient _graphClient;
         private ListCollectionResponse? _listResponse;
 
         public SharepointTableProvider(SharepointSourceOptions sharepointSourceOptions)
         {
             _sharepointSourceOptions = sharepointSourceOptions;
+            _graphClient = new GraphServiceClient(_sharepointSourceOptions.TokenCredential);
         }
         public bool TryGetTableInformation(string tableName, [NotNullWhen(true)] out TableMetadata? tableMetadata)
         {
@@ -41,9 +44,26 @@ namespace FlowtideDotNet.Connector.Sharepoint.Internal
             }
 
             var list = _listResponse.Value.FirstOrDefault(x => x.Name == tableName);
+            if (list == null)
+            {
+                tableMetadata = null;
+                return false;
+            }
+            tableMetadata = GetListData(list);
+            return true;
+        }
 
-            tableMetadata = null;
-            return false;
+        private TableMetadata GetListData(List list)
+        {
+            if (list.Id == null)
+            {
+                throw new ArgumentNullException("List id is null");
+            }
+            if (_tables.TryGetValue(list.Id, out var tableMetadata))
+            {
+                return tableMetadata;
+            }
+            return LoadSharepointListData(list).GetAwaiter().GetResult();
         }
 
         private void TryLoadSharepointData()
@@ -54,11 +74,46 @@ namespace FlowtideDotNet.Connector.Sharepoint.Internal
             }
         }
 
+        private async Task<TableMetadata> LoadSharepointListData(List list)
+        {
+            if (list.Name == null)
+            {
+                throw new ArgumentNullException("List name is null");
+            }
+
+            var graphSite = $"{_sharepointSourceOptions.SharepointUrl}:/sites/{_sharepointSourceOptions.Site}:";
+            var columns = await _graphClient.Sites[graphSite].Lists[list.Id].Columns.GetAsync();
+
+            if (columns == null)
+            {
+                throw new InvalidOperationException("Could not fetch sharepoint list columns");
+            }
+            if (columns.Value == null)
+            {
+                throw new InvalidOperationException("Could not fetch sharepoint list columns");
+            }
+
+            List<string> columnNames = new List<string>();
+
+            foreach (var column in columns.Value)
+            {
+                if (column.Name == null)
+                {
+                    continue;
+                }
+                columnNames.Add(column.Name);
+            }
+            columnNames.Add("_fields");
+
+            var metadata = new TableMetadata(list.Name, columnNames);
+            _tables.Add(list.Name, metadata);
+            return metadata;
+        }
+
         private async Task LoadSharepointData()
         {
-            var graphClient = new GraphServiceClient(_sharepointSourceOptions.TokenCredential);
             var graphSite = $"{_sharepointSourceOptions.SharepointUrl}:/sites/{_sharepointSourceOptions.Site}:";
-            _listResponse = await graphClient.Sites[graphSite].Lists.GetAsync();
+            _listResponse = await _graphClient.Sites[graphSite].Lists.GetAsync();
         }
     }
 }
