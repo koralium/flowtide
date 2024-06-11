70f4560e71cdf65cc9d148c406ac50d9b06b8041
Ulimo <alexander.ostman@hotmail.com>
2024-06-11 20:46:47 +0200

    Add long list

---
+++ src/FlowtideDotNet.Core/ColumnStore/Utils/LongList.cs
@@ -0,0 +1,175 @@
+﻿// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using FlowtideDotNet.Core.ColumnStore.Memory;
+using Google.Protobuf.Reflection;
+using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowtideDotNet.Core.ColumnStore.Utils
+{
+    /// <summary>
+    /// A list of longs that can be used in a column store.
+    /// All values are stored in little endian to allow for fast serialization and deserialization.
+    /// </summary>
+    internal unsafe class LongList : IDisposable
+    {
+        private void* _data;
+        private int _dataLength;
+        private int _length;
+        private bool _disposedValue;
+        private readonly IMemoryAllocator _memoryAllocator;
+
+        public LongList(IMemoryAllocator memoryAllocator)
+        {
+            _data = null;
+            _memoryAllocator = memoryAllocator;
+        }
+
+        public LongList(void* data, int dataLength, int length, IMemoryAllocator memoryAllocator)
+        {
+            _data = data;
+            _dataLength = dataLength;
+            _length = length;
+            _memoryAllocator = memoryAllocator;
+        }
+
+        private void EnsureCapacity(int length)
+        {
+            if (_dataLength < length)
+            {
+                var newLength = length * 2;
+                if (newLength < 64)
+                {
+                    newLength = 64;
+                }
+                var newData = _memoryAllocator.Allocate(newLength, 64);
+                var newDataSpan = new Span<long>(newData, newLength);
+                var oldDataSpan = new Span<long>(_data, _dataLength);
+                oldDataSpan.CopyTo(newDataSpan);
+                _memoryAllocator.Free(_data);
+                _data = newData;
+                _dataLength = newLength;
+            }
+        }
+
+        public Span<long> Span => new Span<long>(_data, _dataLength);
+
+        public void Add(long value)
+        {
+            EnsureCapacity(_length + 1);
+            if (BitConverter.IsLittleEndian)
+            {
+                this.Span[_length++] = value;
+            }
+            else
+            {
+                this.Span[_length++] = BinaryPrimitives.ReverseEndianness(value);
+            }
+        }
+
+        public void InsertAt(int index, long value)
+        {
+            if (index == _length)
+            {
+                Add(value);
+                return;
+            }
+            
+            EnsureCapacity(_length + 1);
+            var span = Span;
+            span.Slice(index, _length - index).CopyTo(span.Slice(index + 1, _length - index));
+            if (BitConverter.IsLittleEndian)
+            {
+                span[index] = value;
+            }
+            else
+            {
+                span[index] = BinaryPrimitives.ReverseEndianness(value);
+            }
+            _length++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            var span = Span;
+            span.Slice(index + 1, _length - index - 1).CopyTo(span.Slice(index, _length - index - 1));
+            _length--;
+        }
+
+        public void RemoveRange(int index, int count)
+        {
+            var span = Span;
+            var length = _length - index - count;
+            span.Slice(index + count, length).CopyTo(span.Slice(index));
+            _length -= count;
+        }
+
+        public long Get(in int index)
+        {
+            var span = Span;
+            return span[index];
+        }
+
+        public void Update(in int index, in long value)
+        {
+            var span = Span;
+            if (BitConverter.IsLittleEndian)
+            {
+                span[index] = value;
+            }
+            else
+            {
+                span[index] = BinaryPrimitives.ReverseEndianness(value);
+            }
+        }
+
+        public long this[int index]
+        {
+            get
+            {
+                return Get(index);
+            }
+            set
+            {
+                Update(index, value);
+            }
+        }
+
+        public int Count => _length;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                _memoryAllocator.Free(_data);
+                _disposedValue = true;
+            }
+        }
+
+        ~LongList()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ tests/FlowtideDotNet.Core.Tests/ColumnStore/Utils/LongListTests.cs
@@ -0,0 +1,144 @@
+﻿// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using FlowtideDotNet.Core.ColumnStore.Memory;
+using FlowtideDotNet.Core.ColumnStore.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowtideDotNet.Core.Tests.ColumnStore.Utils
+{
+    public class LongListTests
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            var allocator = new NativeMemoryAllocator();
+            using var longList = new LongList(allocator);
+            longList.Add(1);
+            longList.Add(2);
+            longList.Add(3);
+
+            Assert.Equal(1, longList[0]);
+            Assert.Equal(2, longList[1]);
+            Assert.Equal(3, longList[2]);
+        }
+
+        [Fact]
+        public void TestInsertMiddle()
+        {
+            var allocator = new NativeMemoryAllocator();
+            using var longList = new LongList(allocator);
+            longList.Add(1);
+            longList.Add(3);
+            longList.InsertAt(1, 2);
+
+            Assert.Equal(1, longList[0]);
+            Assert.Equal(2, longList[1]);
+            Assert.Equal(3, longList[2]);
+        }
+
+        [Fact]
+        public void TestInsertFirst()
+        {
+            var allocator = new NativeMemoryAllocator();
+            using var longList = new LongList(allocator);
+            longList.Add(2);
+            longList.Add(3);
+            longList.InsertAt(0, 1);
+
+            Assert.Equal(1, longList[0]);
+            Assert.Equal(2, longList[1]);
+            Assert.Equal(3, longList[2]);
+        }
+
+        [Fact]
+        public void TestInsertLast()
+        {
+            var allocator = new NativeMemoryAllocator();
+            using var longList = new LongList(allocator);
+            longList.Add(1);
+            longList.Add(2);
+            longList.InsertAt(2, 3);
+
+            Assert.Equal(1, longList[0]);
+            Assert.Equal(2, longList[1]);
+            Assert.Equal(3, longList[2]);
+        }
+
+        [Fact]
+        public void TestRemoveMiddle()
+        {
+            var allocator = new NativeMemoryAllocator();
+            using var longList = new LongList(allocator);
+            longList.Add(1);
+            longList.Add(2);
+            longList.Add(3);
+            longList.RemoveAt(1);
+
+            Assert.Equal(1, longList[0]);
+            Assert.Equal(3, longList[1]);
+            Assert.Equal(2, longList.Count);
+        }
+
+        [Fact]
+        public void TestRemoveFirst()
+        {
+            var allocator = new NativeMemoryAllocator();
+            using var longList = new LongList(allocator);
+            longList.Add(1);
+            longList.Add(2);
+            longList.Add(3);
+            longList.RemoveAt(0);
+
+            Assert.Equal(2, longList[0]);
+            Assert.Equal(3, longList[1]);
+            Assert.Equal(2, longList.Count);
+        }
+
+        [Fact]
+        public void TestRemoveLast()
+        {
+            var allocator = new NativeMemoryAllocator();
+            using var longList = new LongList(allocator);
+            longList.Add(1);
+            longList.Add(2);
+            longList.Add(3);
+            longList.RemoveAt(2);
+
+            Assert.Equal(1, longList[0]);
+            Assert.Equal(2, longList[1]);
+            Assert.Equal(2, longList.Count);
+        }
+
+        [Fact]
+        public void TestRemoveRange()
+        {
+            var allocator = new NativeMemoryAllocator();
+            using var longList = new LongList(allocator);
+            longList.Add(1);
+            longList.Add(2);
+            longList.Add(3);
+            longList.Add(4);
+            longList.Add(5);
+            longList.RemoveRange(1, 2);
+
+            Assert.Equal(1, longList[0]);
+            Assert.Equal(4, longList[1]);
+            Assert.Equal(5, longList[2]);
+            Assert.Equal(3, longList.Count);
+        }
+    }
+}
