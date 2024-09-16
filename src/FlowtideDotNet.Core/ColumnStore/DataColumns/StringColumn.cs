23ba0dc8a4dacf04b7995b02f3fd196e23501de7
Ulimo <alexander.ostman@hotmail.com>
2024-09-16 23:12:04 +0200

    Change sql server to use column data instead of row data when reading from sql server (#507)
    * Start on implementing expression execution on column data
    
    * Fix data value comparer to also work with maps and lists
    
    * Add support to register scalar column functions
    Also added support to compile boolean functions.
    
    * Add filter operator that works with column data
    
    * Change sql server read source to column
    
    * Dispose columns correctly if they were not used
    
    * remove test project reference
    
    * Change to use the new memory allocator in the source
    
    * remove debug write

--- src/FlowtideDotNet.Core/ColumnStore/DataColumns/StringColumn.cs
+++ src/FlowtideDotNet.Core/ColumnStore/DataColumns/StringColumn.cs
@@ -98,7 +98,7 @@ namespace FlowtideDotNet.Core.ColumnStore
         public void GetValueAt(in int index, in DataValueContainer dataValueContainer, in ReferenceSegment? child)
         {
             dataValueContainer._type = ArrowTypeId.String;
-            dataValueContainer._stringValue = new StringValue(_binaryList.Get(in index).ToArray());
+            dataValueContainer._stringValue = new StringValue(_binaryList.GetMemory(in index));
         }
 
         public void InsertAt<T>(in int index, in T value) where T : IDataValue
