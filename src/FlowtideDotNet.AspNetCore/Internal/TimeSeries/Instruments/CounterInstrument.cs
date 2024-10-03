1e596318e4f9e090efbeb033bc3a0597a4881eb1
Ulimo <alexander.ostman@hotmail.com>
2024-10-03 22:28:21 +0200

    Fix in metrics so readonlyspan of tags are used directly to find the correct metric series

--- src/FlowtideDotNet.AspNetCore/Internal/TimeSeries/Instruments/CounterInstrument.cs
+++ src/FlowtideDotNet.AspNetCore/Internal/TimeSeries/Instruments/CounterInstrument.cs
@@ -10,6 +10,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using FlowtideDotNet.AspNetCore.Internal.TimeSeries.Utils;
 using System.Collections.Concurrent;
 using System.Text;
 
@@ -18,7 +19,9 @@ namespace FlowtideDotNet.AspNetCore.TimeSeries.Instruments
     internal class CounterInstrument : IMetricInstrument
     {
         private readonly string name;
-        private ConcurrentDictionary<string, MetricValue> _values = new ConcurrentDictionary<string, MetricValue>();
+        private List<KeyValuePair<string, string>[]> _tagsList = new List<KeyValuePair<string, string>[]>();
+        private List<MetricValue> _metrics = new List<MetricValue>();
+        private ReaderWriterLockSlim _rwLock = new ReaderWriterLockSlim();
 
         public CounterInstrument(string name)
         {
@@ -27,37 +30,57 @@ namespace FlowtideDotNet.AspNetCore.TimeSeries.Instruments
 
         public void Record(ReadOnlySpan<KeyValuePair<string, object?>> tags, double value)
         {
-            Dictionary<string, string> labels = new Dictionary<string, string>();
+            _rwLock.EnterReadLock();
+            int index = TagListUtils.BinarySearchList(tags, _tagsList);
 
-            foreach (var tag in tags)
+            if (index >= 0)
             {
-                labels.Add(tag.Key, tag.Value.ToString()!);
-            }
-
-            var key = string.Join(',', labels.Select(t => $"{t.Key}={t.Value}"));
-
-            if (!_values.TryGetValue(key, out var metricValue))
-            {
-                metricValue = new MetricValue(labels.ToDictionary(x => x.Key.Replace(".", "_"), x => x.Value));
-                metricValue.Value = value;
-                _values.AddOrUpdate(key, metricValue, (k ,v) =>
-                {
-                    v.Value += value;
-                    return v;
-                });
+                _metrics[index].Value += value;
+                _rwLock.ExitReadLock();
             }
             else
             {
-                metricValue.Value += value;
+                _rwLock.ExitReadLock();
+                _rwLock.EnterWriteLock();
+                index = TagListUtils.BinarySearchList(tags, _tagsList);
+
+                if (index >= 0)
+                {
+                    _metrics[index].Value += value;
+                    _rwLock.ExitWriteLock();
+                    return;
+                }
+
+                // Not existing
+                index = ~index;
+
+                var tagList = new KeyValuePair<string, string>[tags.Length];
+
+                for (int i = 0; i < tags.Length; i++)
+                {
+                    tagList[i] = new KeyValuePair<string, string>(tags[i].Key, tags[i].Value!.ToString()!);
+                }
+                    
+                var metricValue = new MetricValue(tagList.ToDictionary(x => x.Key.Replace(".", "_"), x => x.Value));
+                metricValue.Value = value;
+
+                _tagsList.Insert(index, tagList);
+                _metrics.Insert(index, metricValue);
+
+                _rwLock.ExitWriteLock();
             }
         }
 
         public async ValueTask StoreMeasurements(long timestamp, MetricSeries series)
         {
-            foreach(var value in _values)
+            _rwLock.EnterReadLock();
+
+            for (int i = 0; i < _metrics.Count; i++)
             {
-                await series.SetValueToSerie(name, value.Value.Tags, timestamp, value.Value.Value);
+                var metric = _metrics[i];
+                await series.SetValueToSerie(name, metric.Tags, timestamp, metric.Value);
             }
+            _rwLock.ExitReadLock();
         }
     }
 }
