4083ec335782125f4ec9daa3a966730afe05f130
Ulimo <alexander.ostman@hotmail.com>
2024-10-30 21:34:29 +0100

    Change console sink to work similar to other connectors
    It outputs data first on the initial checkpoint and then on following watermarks.

--- src/FlowtideDotNet.Core/Sinks/Console/ConsoleSink.cs
+++ src/FlowtideDotNet.Core/Sinks/Console/ConsoleSink.cs
@@ -10,9 +10,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using FlowtideDotNet.Base;
 using FlowtideDotNet.Base.Vertices.Egress;
+using FlowtideDotNet.Core.ColumnStore.TreeStorage;
+using FlowtideDotNet.Storage.Serializers;
 using FlowtideDotNet.Storage.StateManager;
+using FlowtideDotNet.Storage.Tree;
 using FlowtideDotNet.Substrait.Relations;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks.Dataflow;
 
 namespace FlowtideDotNet.Core.Sinks
@@ -20,6 +27,8 @@ namespace FlowtideDotNet.Core.Sinks
     internal class ConsoleSink : EgressVertex<StreamEventBatch, object>
     {
         private readonly WriteRelation writeRelation;
+        private IBPlusTree<ColumnRowReference, int, ColumnKeyStorageContainer, ListValueContainer<int>>? _tree;
+        private bool m_initialDataSent;
 
         public ConsoleSink(WriteRelation writeRelation, ExecutionDataflowBlockOptions executionDataflowBlockOptions) : base(executionDataflowBlockOptions)
         {
@@ -38,41 +47,98 @@ namespace FlowtideDotNet.Core.Sinks
             return Task.CompletedTask;
         }
 
-        protected override Task InitializeOrRestore(long restoreTime, object? state, IStateManagerClient stateManagerClient)
+        protected override async Task InitializeOrRestore(long restoreTime, object? state, IStateManagerClient stateManagerClient)
         {
-            return Task.CompletedTask;
-        }
-
-        protected override Task<object> OnCheckpoint(long checkpointTime)
-        {
-            return Task.FromResult(new object());
-        }
-
-        protected override Task OnRecieve(StreamEventBatch msg, long time)
-        {
-            var consoleTable = new ConsoleTable(writeRelation.TableSchema.Names.Prepend("Weight").ToArray());
-            foreach (var e in msg.Events)
+            _tree = await stateManagerClient.GetOrCreateTree("tree", new BPlusTreeOptions<ColumnRowReference, int, ColumnKeyStorageContainer, ListValueContainer<int>>()
             {
-                var vals = new object[writeRelation.TableSchema.Names.Count + 1];
-                vals[0] = e.Weight;
+                Comparer = new ColumnComparer(writeRelation.OutputLength),
+                KeySerializer = new ColumnStoreSerializer(writeRelation.OutputLength, MemoryAllocator),
+                MemoryAllocator = MemoryAllocator,
+                UseByteBasedPageSizes = true,
+                ValueSerializer = new ValueListSerializer<int>(new IntSerializer())
+            });
+        }
 
-                for (int i = 0; i < e.Length; i++)
+        protected override async Task<object> OnCheckpoint(long checkpointTime)
+        {
+            if (!m_initialDataSent)
+            {
+                await WriteData();
+                m_initialDataSent = true;
+            }
+            return new object();
+        }
+
+        protected override async Task OnWatermark(Watermark watermark)
+        {
+            if (m_initialDataSent)
+            {
+                await WriteData();
+            }
+        }
+
+        private async Task WriteData()
+        {
+            Debug.Assert(_tree != null);
+
+            var consoleTable = new ConsoleTable(writeRelation.TableSchema.Names.Prepend("Weight").ToArray());
+
+            using var iterator = _tree.CreateIterator();
+            await iterator.SeekFirst();
+
+            using MemoryStream memoryStream = new MemoryStream();
+            Utf8JsonWriter writer = new Utf8JsonWriter(memoryStream);
+            
+            await foreach(var page in iterator)
+            {
+                foreach(var kv in page)
                 {
-                    vals[i + 1] = e.GetColumn(i).ToJson;
-                }
+                    var vals = new object[writeRelation.TableSchema.Names.Count + 1];
+                    vals[0] = kv.Value;
 
-                consoleTable.AddRow(vals);
-                //if (e.Weight > 0)
-                //{
-                //    Console.WriteLine($"w: +{e.Weight}, data: {e.Vector.ToJson}");
-                //}
-                //else
-                //{
-                //    Console.WriteLine($"w: -{e.Weight}, data: {e.Vector.ToJson}");
-                //}
+                    for (int i = 0; i < writeRelation.TableSchema.Names.Count; i++)
+                    {
+                        kv.Key.referenceBatch.Columns[i].WriteToJson(in writer, kv.Key.RowIndex);
+                        await writer.FlushAsync();
+                        vals[i + 1] = Encoding.UTF8.GetString(memoryStream.ToArray());
+                        memoryStream.Position = 0;
+                        writer.Reset();
+                    }
+
+                    consoleTable.AddRow(vals);
+                }
             }
             consoleTable.Write(Format.Default);
-            return Task.CompletedTask;
+            await _tree.Clear();
+        }
+
+
+        protected override async Task OnRecieve(StreamEventBatch msg, long time)
+        {
+            Debug.Assert(_tree != null);
+
+            for (int i = 0; i < msg.Data.Count; i++)
+            {
+                var row = new ColumnRowReference()
+                {
+                    referenceBatch = msg.Data.EventBatchData,
+                    RowIndex = i
+                };
+
+                await _tree.RMWNoResult(row, msg.Data.Weights[i], (input, existing, found) =>
+                {
+                    if (found)
+                    {
+                        existing += input;
+                        if (existing == 0)
+                        {
+                            return (existing, GenericWriteOperation.Delete);
+                        }
+                        return (existing, GenericWriteOperation.Upsert);
+                    }
+                    return (input, GenericWriteOperation.Upsert);
+                });
+            }
         }
     }
 }
