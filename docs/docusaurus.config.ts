9771c2f131ef145d1f68324ef40038d750847cc7
Alexander Östman <alexander.ostman@hotmail.com>
2024-11-14 11:28:22 +0100

    Prepare for release 0.11.0
    Add blog post explaining the bigger changes

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -53,6 +53,8 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
+          //blogTitle: 'Releases',
+          //routeBasePath: 'releases',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -85,7 +87,7 @@ const config: Config = {
           position: 'left',
           label: 'Documentation',
         },
-        // {to: '/blog', label: 'Blog', position: 'left'},
+        {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/koralium/flowtide',
           label: 'GitHub',
