9caa47bc7571ea66b019fd84a50709e1833060f0
Ulimo <alexander.ostman@hotmail.com>
2024-11-17 16:17:43 +0100

    Add support for catalogs (#583)
    * Add support for catalogs
    
    This also adds a sample with catalogs with sql server to sql server, some minor changes also to SQL server connector to support referencing cross database.
    
    * Fix the sample, to include data generation for an endless stream

--- samples/AspireSamples/Entities/Order.cs
+++ samples/AspireSamples/Entities/Order.cs
@@ -10,31 +10,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using FlowtideDotNet.Core.Connectors;
-using FlowtideDotNet.Substrait.Relations;
-using FlowtideDotNet.Substrait.Sql;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FlowtideDotNet.Core
+namespace AspireSamples.Entities
 {
-    public interface IConnectorManager
+    internal class Order
     {
-        void AddSource(IConnectorSourceFactory connectorSourceFactory);
+        [Key]
+        public int OrderKey { get; set; }
 
-        void AddSink(IConnectorSinkFactory connectorSinkFactory);
+        public int UserKey { get; set; }
 
-        void AddCatalog(string catalogName, Action<ICatalogConnectorManager> options);
-
-        IConnectorSourceFactory GetSourceFactory(ReadRelation readRelation);
-
-        IConnectorSinkFactory GetSinkFactory(WriteRelation writeRelation);
-
-        IEnumerable<ITableProvider> GetTableProviders();
-
-        void AddTableProvider(ITableProvider tableProvider);
+        public DateTime Orderdate { get; set; }
     }
 }
