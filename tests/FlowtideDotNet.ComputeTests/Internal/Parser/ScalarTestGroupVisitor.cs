2cc33097aef40f8c029e22dcb0e0ec93321f63e3
Ulimo <alexander.ostman@hotmail.com>
2025-03-13 19:47:07 +0100

    Refactor compute tests to use xunit test framework instead of source generator (#718)
    This allows to link to test case and not being dependent on source generation.

---
+++ tests/FlowtideDotNet.ComputeTests/Internal/Parser/ScalarTestGroupVisitor.cs
@@ -0,0 +1,44 @@
+﻿// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Antlr4.Runtime.Misc;
+using FlowtideDotNet.ComputeTests.Internal.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowtideDotNet.ComputeTests.Internal.Tests
+{
+    internal class ScalarTestGroupVisitor : FuncTestCaseParserBaseVisitor<TestGroup>
+    {
+        public override TestGroup VisitScalarFuncTestGroup([NotNull] FuncTestCaseParser.ScalarFuncTestGroupContext context)
+        {
+            var description = TestGroupDescriptionParser.ParseTestGroupDescription(context.testGroupDescription());
+
+            var scalarTests = context.testCase();
+
+            List<TestCase> testCases = new List<TestCase>();
+
+            foreach (var scalarTest in scalarTests)
+            {
+                testCases.Add(new TestCase()
+                {
+                    Text = scalarTest.GetText(),
+                    LineNumber = scalarTest.Start.Line
+                });
+            }
+            return new TestGroup(description, testCases);
+        }
+    }
+}
---
+++ tests/FlowtideDotNet.ComputeTests/Internal/Parser/Tests/ResultParser.cs
@@ -0,0 +1,37 @@
+﻿// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using FlowtideDotNet.ComputeTests.Internal.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowtideDotNet.ComputeTests.Internal.Tests
+{
+    internal static class ResultParser
+    {
+        public static ExpectedResult ParseExpectedResult(FuncTestCaseParser.ResultContext context)
+        {
+
+            var value = context.argument();
+            if (value != null)
+            {
+                var dataValue = ArgumentParser.ParseArgument(value);
+                return new ExpectedResult(dataValue, false);
+            }
+
+            return new ExpectedResult(null, true);
+        }
+    }
+}
