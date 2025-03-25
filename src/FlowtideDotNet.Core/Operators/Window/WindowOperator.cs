73a9bda5040defcd0e9cdf621982965524c61dcd
Alexander Östman <alexander.ostman@hotmail.com>
2025-03-25 14:16:51 +0100

    Start on window operator

---
+++ src/FlowtideDotNet.Core/Operators/Window/WindowOperator.cs
@@ -0,0 +1,158 @@
+﻿// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using FlowtideDotNet.Base;
+using FlowtideDotNet.Base.Vertices.Unary;
+using FlowtideDotNet.Core.ColumnStore.Comparers;
+using FlowtideDotNet.Core.ColumnStore.TreeStorage;
+using FlowtideDotNet.Core.Compute.Columnar;
+using FlowtideDotNet.Core.Compute;
+using FlowtideDotNet.Storage.StateManager;
+using FlowtideDotNet.Substrait.Relations;
+using System.Threading.Tasks.Dataflow;
+using FlowtideDotNet.Storage.Tree;
+using FlowtideDotNet.Storage.Serializers;
+using FlowtideDotNet.Substrait.Expressions;
+using FlowtideDotNet.Core.ColumnStore;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
+
+namespace FlowtideDotNet.Core.Operators.Window
+{
+    internal class WindowOperator : UnaryVertex<StreamEventBatch>
+    {
+        private readonly ConsistentPartitionWindowRelation _relation;
+        private IColumnComparer<ColumnRowReference>? _sortComparer;
+        private IBPlusTree<ColumnRowReference, int, ColumnKeyStorageContainer, PrimitiveListValueContainer<int>>? _persistentTree;
+        private List<int> _partitionColumns;
+        private List<Action<EventBatchData, int, Column>> _partitionCalculateExpressions;
+
+        public WindowOperator(ConsistentPartitionWindowRelation relation, IFunctionsRegister functionsRegister, ExecutionDataflowBlockOptions executionDataflowBlockOptions) : base(executionDataflowBlockOptions)
+        {
+            _relation = relation;
+            if (_relation.WindowFunctions.Count > 1)
+            {
+                throw new NotSupportedException("Only one window function is supported at this time per window operator");
+            }
+            // Create the comparer for the ordering
+            if (relation.OrderBy.Count > 0)
+            {
+                _sortComparer = SortFieldCompareCompiler.CreateComparer(relation.OrderBy, functionsRegister);
+            }
+            CreatePartitionColumnExpressions(functionsRegister);
+        }
+
+        [MemberNotNull(nameof(_partitionColumns), nameof(_partitionCalculateExpressions))]
+        private void CreatePartitionColumnExpressions(IFunctionsRegister functionsRegister)
+        {
+            List<int> partitionColumns = new List<int>();
+            int extraColumnCounter = _relation.Input.OutputLength;
+            List<Action<EventBatchData, int, Column>> partitionCalculateExpressions = new List<Action<EventBatchData, int, Column>>();
+            foreach (var partitionExpr in _relation.PartitionBy)
+            {
+                if (partitionExpr is DirectFieldReference directFieldReference &&
+                    directFieldReference.ReferenceSegment is StructReferenceSegment structReferenceSegment)
+                {
+                    partitionColumns.Add(structReferenceSegment.Field);
+                }
+                else
+                {
+                    var exprFunc = ColumnProjectCompiler.Compile(partitionExpr, functionsRegister);
+                    partitionCalculateExpressions.Add(exprFunc);
+                    partitionColumns.Add(extraColumnCounter++);
+                }
+            }
+            _partitionColumns = partitionColumns;
+            _partitionCalculateExpressions = partitionCalculateExpressions;
+        }
+
+        public override string DisplayName => "WindowOperator";
+
+        public override Task Compact()
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task DeleteAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task OnCheckpoint()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override IAsyncEnumerable<StreamEventBatch> OnWatermark(Watermark watermark)
+        {
+            return base.OnWatermark(watermark);
+        }
+
+        public override async IAsyncEnumerable<StreamEventBatch> OnRecieve(StreamEventBatch msg, long time)
+        {
+            Debug.Assert(_persistentTree != null);
+            // Need to calculate any partition expressions into values if they are not already
+            Column[] extraPartitionColumns = new Column[_partitionCalculateExpressions.Count];
+
+            for (int i = 0; i < extraPartitionColumns.Length; i++)
+            {
+                extraPartitionColumns[i] = new Column(MemoryAllocator);
+            }
+
+            IColumn[] columns = new IColumn[msg.Data.EventBatchData.Columns.Count + extraPartitionColumns.Length];
+            for (int i = 0; i < msg.Data.EventBatchData.Columns.Count; i++)
+            {
+                columns[i] = msg.Data.EventBatchData.Columns[i];
+            }
+            for (int i = 0; i < extraPartitionColumns.Length; i++)
+            {
+                columns[msg.Data.EventBatchData.Columns.Count + i] = extraPartitionColumns[i];
+            }
+            var eventBatchWithPartitionColumns = new EventBatchData(columns);
+
+            for (int i = 0; i < msg.Data.Weights.Count; i++)
+            {
+                // Calculate partition values that are more complex than simply a column value
+                // These values are stored in the extraPartitionColumns array
+                for (int k = 0; k < _partitionCalculateExpressions.Count; k++)
+                {
+                    _partitionCalculateExpressions[k](msg.Data.EventBatchData, i, extraPartitionColumns[k]);
+                }
+
+                var rowRef = new ColumnRowReference()
+                {
+                    referenceBatch = eventBatchWithPartitionColumns,
+                    RowIndex = i
+                };
+
+                await _persistentTree.RMWNoResult(rowRef, 0, (input, current, exist) =>
+                {
+                    return (input, GenericWriteOperation.Upsert);
+                });
+            }
+
+            throw new NotImplementedException();
+        }
+
+        protected override async Task InitializeOrRestore(IStateManagerClient stateManagerClient)
+        {
+            _persistentTree = await stateManagerClient.GetOrCreateTree("persistent",
+                new FlowtideDotNet.Storage.Tree.BPlusTreeOptions<ColumnRowReference, int, ColumnKeyStorageContainer, PrimitiveListValueContainer<int>>()
+                {
+                    Comparer = new WindowInsertComparer(_sortComparer, new List<int>(), new List<int>()),
+                    KeySerializer = new ColumnStoreSerializer(_relation.Input.OutputLength + _partitionCalculateExpressions.Count, MemoryAllocator),
+                    MemoryAllocator = MemoryAllocator,
+                    ValueSerializer = new PrimitiveListValueContainerSerializer<int>(MemoryAllocator)
+                });
+        }
+    }
+}
