83b5ccdb17128c95f4f2a83add24108d0906a536
Ulimo <alexander.ostman@hotmail.com>
2025-10-29 13:53:13 +0100

    fix feedback

--- src/FlowtideDotNet.Core/Operators/Read/ColumnBatchReadBaseOperator.cs
+++ src/FlowtideDotNet.Core/Operators/Read/ColumnBatchReadBaseOperator.cs
@@ -369,21 +369,11 @@ namespace FlowtideDotNet.Core.Operators.Read
                             {
                                 if (exists)
                                 {
-                                    bool updated = false;
-                                    bool hasChanges = false;
-                                    for (int k = 0; k < _otherColumns.Count; k++)
-                                    {
-                                        var compareResult = input.referenceBatch.Columns[_otherColumns[k]].CompareTo(current.referenceBatch.Columns[k], input.RowIndex, current.RowIndex);
-                                        if (compareResult != 0)
-                                        {
-                                            hasChanges = true;
-                                            break;
-                                        }
-                                    }
-                                    if (!hasChanges)
+                                    if (CompareRowReference(input, current) == 0)
                                     {
                                         return (current, GenericWriteOperation.None);
                                     }
+                                    bool updated = false;
                                     if (_filter == null || _filter(input.referenceBatch, input.RowIndex))
                                     {
                                         weights.Add(1);
