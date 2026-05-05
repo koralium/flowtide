7263074345e0d6861f2f5db02926a7cf1b335b89
Ulimo <alexander.ostman@hotmail.com>
2026-05-05 10:57:20 +0200

    Fix bulk in interface

--- src/FlowtideDotNet.Storage/Tree/IBplusTreeComparer.cs
+++ src/FlowtideDotNet.Storage/Tree/IBplusTreeComparer.cs
@@ -10,6 +10,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Microsoft.Extensions.Logging;
+
 namespace FlowtideDotNet.Storage.Tree
 {
     public struct FindBoundriesResult
@@ -36,19 +38,20 @@ namespace FlowtideDotNet.Storage.Tree
         /// <returns></returns>
         int FindIndex(in K key, in TKeyContainer keyContainer);
 
-        void FindBoundriesBulk(ReadOnlySpan<K> keys, in TKeyContainer keyContainer, Span<int> lowerBounds, Span<int> upperBounds)
+        void FindBoundriesBulk(ReadOnlySpan<K> keys, ReadOnlySpan<int> sortedLookup, in TKeyContainer keyContainer, Span<int> lowerBounds, Span<int> upperBounds)
         {
             int currentStart = 0;
             int maxEnd = keyContainer.Count - 1;
 
-            for (int i = 0; i < keys.Length; i++)
+            for (int i = 0; i < sortedLookup.Length; i++)
             {
-                var bounds = FindBoundries(keys[i], in keyContainer, currentStart, maxEnd);
+                var keyIndex = sortedLookup[i];
+                var bounds = FindBoundries(keys[keyIndex], in keyContainer, currentStart, maxEnd);
 
                 lowerBounds[i] = bounds.lowerBounds;
                 upperBounds[i] = bounds.upperBounds;
 
-                currentStart = bounds.lowerBounds;
+                currentStart = bounds.lowerBounds < 0 ? ~bounds.lowerBounds : bounds.lowerBounds;
             }
         }
 
--- tests/FlowtideDotNet.Benchmarks/BoundarySearchHybridInt32Benchmarks.cs
+++ tests/FlowtideDotNet.Benchmarks/BoundarySearchHybridInt32Benchmarks.cs
@@ -122,7 +122,7 @@ namespace FlowtideDotNet.Benchmarks
         public void HybridInt32()
         {
             ResetBounds();
-            BoundarySearchHybridPrimitiveNoNull.SearchBoundries_Hybrid<int>(
+            BoundarySearchHybridPrimitiveNoNull<int>.SearchBoundries_Hybrid(
                 _treeColumn,
                 _inputColumn,
                 _inputSortedLookup,
