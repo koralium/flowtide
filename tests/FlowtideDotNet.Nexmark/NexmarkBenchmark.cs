3ced86c464b63eff61ee3c0ca661e4565dc93e57
Ulimo <alexander.ostman@hotmail.com>
2026-05-20 15:43:05 +0200

    Add extra source

--- tests/FlowtideDotNet.Nexmark/NexmarkBenchmark.cs
+++ tests/FlowtideDotNet.Nexmark/NexmarkBenchmark.cs
@@ -29,7 +29,7 @@ namespace FlowtideDotNet.Nexmark
         [GlobalSetup]
         public void Setup()
         {
-            _generator = new NexmarkGenerator(1_000_000, batchSize: 10_000);
+            _generator = new NexmarkGenerator(10_000_000, batchSize: 10_000);
             _dataStream = _generator.Generate();
         }
 
