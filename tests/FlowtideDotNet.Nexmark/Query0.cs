f9f0db131a31ae9ce79f738519fb24179c973ba1
Ulimo <alexander.ostman@hotmail.com>
2026-07-18 23:37:35 +0200

    Add cache hit diagnoser in nexmark benchmark to see cache results for tuning

--- tests/FlowtideDotNet.Nexmark/Query0.cs
+++ tests/FlowtideDotNet.Nexmark/Query0.cs
@@ -21,6 +21,7 @@ using System.Threading.Tasks;
 namespace FlowtideDotNet.Nexmark
 {
     [EventCountDiagnoser]
+    [CacheHitRateDiagnoser]
     public class Query0 : QueryBase
     {
         [Benchmark]
